04ad382dee7115a838e033053bc28a29adc7edc9
Jan Wichelmann <j.wichelmann@uni-luebeck.de>
2020-11-16 10:11:13 +0100

    Draft for GDB tracer

---
+++ GdbTracer/GdbTracer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GdbTracer
+{
+    public class GdbTracer // TODO: <TDebugger> where TDebugger : ...
+    {
+        /* Required features:
+         Identify begin and end of testcase
+         Identify begin and end of images
+         Identify begin and end of stack
+         Track allocations
+         Optional: Intercept RDRAND?
+         Trace:
+           Branches
+           Memory accesses
+           
+           
+           
+          -> Build communication between VM and debugger client...
+         */
+    }
+}
